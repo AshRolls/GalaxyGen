523afc2ba3671f26297f063b10c889ba2748582b
AshRolls <ashforcash@gmail.com>
2016-03-20 22:35:20 +0000

    start a new project for testing

---
+++ GalaxyGenTests/UT_Galaxy.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyGenTests
+{
+    [TestFixture]
+    public class UT_Galaxy
+    {
+        [Test]
+        public void Galaxy_Exists()
+        {
+
+        }
+
+    }
+}
