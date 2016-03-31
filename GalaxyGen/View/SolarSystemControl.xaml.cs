dc943999bd2d8859af0321fe1a9ec39263fdf789
AshRolls <ashforcash@gmail.com>
2016-03-31 22:09:35 +0100

    move the visualistation into a control and turn the sun back on. TURN THE SUN BACK ON.

---
+++ GalaxyGen/View/SolarSystemControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using GalaxyGen.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GalaxyGen
+{
+    /// <summary>
+    /// Interaction logic for SolarSystemControl.xaml
+    /// </summary>
+    public partial class SolarSystemControl : UserControl
+    {
+        public SolarSystemControl()
+        {
+            InitializeComponent();
+        }
+
+        public ISolarSystemViewModel SolarSystemVm
+        {
+            get { return (ISolarSystemViewModel)GetValue(SolarSystemVmProperty); }
+            set { SetValue(SolarSystemVmProperty, value); }
+        }
+
+        public static readonly DependencyProperty SolarSystemVmProperty =
+            DependencyProperty.Register("SolarSystemVm", typeof(ISolarSystemViewModel), typeof(SolarSystemControl));
+
+        public IPlanetViewModel PlanetVm
+        {
+            get { return (IPlanetViewModel)GetValue(PlanetVmProperty); }
+            set { SetValue(PlanetVmProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlanetVmProperty =
+            DependencyProperty.Register("PlanetVm", typeof(IPlanetViewModel), typeof(SolarSystemControl));
+    }
+}
