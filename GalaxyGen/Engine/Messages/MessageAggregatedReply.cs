545946c0981cc8247ec924a2ab62dca99a7a1639
Ashley Rolleston <ashley.rolleston@mojomaritime.com>
2016-10-17 12:22:48 +0100

    Add a new class for when we want to gather all replies

---
+++ GalaxyGen/Engine/Messages/MessageAggregatedReply.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace GalaxyGen.Engine.Messages
+{
+    internal class MessageAggregatedReply<T>
+    {
+        internal List<T> replies { get; private set; }
+
+        public MessageAggregatedReply(List<T> replies)
+        {
+            this.replies = replies;
+        }
+    }
+}
---
+++ GalaxyGen/Engine/Messages/MessageAggregator.cs
@@ -0,0 +1,47 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyGen.Engine.Messages
+{
+    class MessageAggregator<T> : ReceiveActor
+    {
+        private IActorRef originalSender;
+        private ISet<IActorRef> refs;
+
+        public MessageAggregator(ISet<IActorRef> refs)
+        {
+            this.refs = refs;
+            // this operation will finish after 30 sec of inactivity
+            // (when no new message arrived)
+            Context.SetReceiveTimeout(TimeSpan.FromSeconds(30));
+            ReceiveAny(x =>
+            {
+                originalSender = Sender;
+                foreach (var aref in refs) aref.Tell(x);
+                Become(Aggregating);
+            });
+        }
+
+        private void Aggregating()
+        {
+            var replies = new List<T>();
+            // when timeout occurred, we reply with what we've got so far
+            Receive<ReceiveTimeout>(_ => ReplyAndStop(replies));
+            Receive<T>(x =>
+            {
+                if (refs.Remove(Sender)) replies.Add(x);
+                if (refs.Count == 0) ReplyAndStop(replies);
+            });
+        }
+
+        private void ReplyAndStop(List<T> replies)
+        {
+            originalSender.Tell(new MessageAggregatedReply<T>(replies));
+            Context.Stop(Self);
+        }
+    }
+}
